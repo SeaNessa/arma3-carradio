dbc5faf0b241e4b7e87d8189c04617729e59b54b
Sean <Sean@Unitedsb.net>
2015-08-27 20:41:22 +0200

    Update and rename radioui.hpp to ui.hpp

--- dialog/ui.hpp
+++ dialog/ui.hpp
@@ -1,12 +1,12 @@
-	class radioHUD
+	class playerHUD
    	{
 		idd=-1;
 		movingEnable=0;
 	  	fadein=0;
 		duration = 99999999999999999999999999999999999999999999;
 	  	fadeout=0;
-		name="radioHUD";
-		onLoad="uiNamespace setVariable ['radioHUD',_this select 0]";
+		name="playerHUD";
+		onLoad="uiNamespace setVariable ['playerHUD',_this select 0]";
 		objects[]={};
 		
 		class controlsBackground 
